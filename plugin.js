23a04e4a6bf20df14db80d7a13597ad48376fc83
IlyaBOT <isarbeu@gmail.com>
2023-02-14 23:37:36 +0300

    Upload theme

---
+++ plugin.js
@@ -0,0 +1,5 @@
+exports.version = 1.0
+exports.description = "Space Gray theme by IlyaBOT"
+exports.apiRequired = 1
+exports.repo = "IlyaBOT/hfs-space-gray-theme"
+exports.frontend_css = 'style.css'
